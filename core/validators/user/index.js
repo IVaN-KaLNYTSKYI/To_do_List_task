12ba19f9b93f48bfd4291dee2a7efba9da2d0110
IVaN-KaLNYTSKYI <68811887+IVaN-KaLNYTSKYI@users.noreply.github.com>
2021-08-17 10:10:16 +0300

    init commit version 0.2

--- core/validators/user/index.js
+++ core/validators/user/index.js
@@ -1,5 +1,8 @@
 module.exports = {
     createValidator: require('./create-user.validator'),
     updateValidator: require('./update-user.validator'),
-    idValidator: require('./id-user.validator')
+    idValidator: require('./id-user.validator'),
+    emailValidator: require('./email-validator'),
+    changePassword: require('./changePassword'),
+    forgotPasswordValidator: require('./forgot-password-validator'),
 };
